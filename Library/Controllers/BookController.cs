c617580a24a2fab0f50f8f12a552d7c0884d7e81
Rosen Roydev <rosen.roydev@gmail.com>
2024-02-16 16:30:18 +0200

    Action All of controller Book is successifully added

---
+++ Library/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly LibraryDbContext context;
+
+        public BookController(LibraryDbContext _context)
+        {
+            context = _context;
+        }
+        public async Task  <IActionResult> All()
+        {
+            var model = await context.Books
+                .Select(b=> new BookAllViewModel
+                {
+                   ImageUrl = b.ImageUrl,
+                   Title = b.Title,
+                   Author = b.Author,
+                   Rating = b.Rating,
+                   Category = b.Category.Name
+                })
+                .AsNoTracking()
+                .ToListAsync();
+            return View(model);
+        }
+    }
+}
---
+++ Library/Models/BookAllViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Models
+{
+    public class BookAllViewModel
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Author {  get; set; } = string.Empty;
+        public decimal Rating { get; set; }
+        public string Category { get; set; } = string.Empty;
+
+    }
+}
